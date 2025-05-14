750e5a3be5ea33f49d7932b43d36b24ec653b167
Dennis Martin Cual <denniscual0716@gmail.com>
2025-05-14 15:47:35 +0800

    fix value type when updating the repository variable

--- src/main.js
+++ src/main.js
@@ -1,26 +1,97 @@
 import * as core from '@actions/core'
+import { Octokit } from '@octokit/core'
 
-/**
- * The main function for the action.
- *
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 export async function run() {
   try {
-    const ms = core.getInput('milliseconds')
+    const commitSha = core.getInput('commit_sha')
+    const personalAccessToken = core.getInput('personal_access_token')
+    const owner = core.getInput('owner')
+    const repo = core.getInput('repo')
+    const label = core.getInput('label')
+    const forceUpdateBuildCountName = core.getInput(
+      'force_update_build_count_name'
+    )
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Commit SHA: ${commitSha}`)
+    core.debug(`Owner: ${owner}`)
+    core.debug(`Repo: ${repo}`)
+    core.debug(`Label: ${label}`)
+    core.debug(`Force Update Build Count Name: ${forceUpdateBuildCountName}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = new Octokit({
+      auth: personalAccessToken
+    })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // Make request to get pull requests associated with the commit
+    core.debug(`Fetching pull requests for commit ${commitSha}...`)
+    const getPullRequestsResponse = await octokit.request(
+      'GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls',
+      {
+        owner,
+        repo,
+        commit_sha: commitSha,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }
+    )
+    // Filter pull requests by label
+    const pullRequests = getPullRequestsResponse.data.filter((pr) => {
+      if (pr.labels.length === 0) {
+        return false
+      }
+      return Boolean(pr.labels.find((prLabel) => prLabel.name === label))
+    })
+
+    const getRepositoryVariableResponse = await octokit.request(
+      'GET /repos/{owner}/{repo}/actions/variables/{name}',
+      {
+        owner,
+        repo,
+        name: forceUpdateBuildCountName,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }
+    )
+    const forceUpdateBuildCount = getRepositoryVariableResponse.data.value
+
+    core.debug(
+      `Force update build count: ${forceUpdateBuildCount} (from variable ${forceUpdateBuildCountName})`
+    )
+    core.debug(
+      `Found ${pullRequests.length} pull requests associated with the commit`
+    )
+
+    // If there are no pull requests associated with the commit, set the output to current force update build count.
+    if (pullRequests.length === 0) {
+      core.setOutput('force_update_build_count', forceUpdateBuildCount)
+      return
+    }
+
+    const latestForceUpdateBuildCount = (
+      parseInt(forceUpdateBuildCount) + 1
+    ).toString()
+    // Update the force update build count variable
+    await octokit.request(
+      'PATCH /repos/{owner}/{repo}/actions/variables/{name}',
+      {
+        owner,
+        repo,
+        name: forceUpdateBuildCountName,
+        value: latestForceUpdateBuildCount,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }
+    )
+    core.setOutput('force_update_build_count', latestForceUpdateBuildCount)
   } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.error(error)
+    }
+
+    // Avoid failing the workflow run if there is an error occurring in the action. Instead, set output to 0.
+    core.setOutput('force_update_build_count', 0)
   }
 }
