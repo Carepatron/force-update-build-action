708f139eeb77b0d77405ad5c53ffb862a18a289e
Dennis Martin Cual <denniscual0716@gmail.com>
2025-05-15 14:11:44 +0800

    set the output to NaN or current force update build count when error is encountered

--- src/main.js
+++ src/main.js
@@ -2,85 +2,108 @@ import * as core from '@actions/core'
 import { Octokit } from '@octokit/rest'
 
 export async function run() {
-  try {
-    const commitSha = core.getInput('commit_sha')
-    const personalAccessToken = core.getInput('personal_access_token')
-    const owner = core.getInput('owner')
-    const repo = core.getInput('repo')
-    const label = core.getInput('label')
-    const forceUpdateBuildCountName = core.getInput(
-      'force_update_build_count_name'
-    )
+	try {
+		const commitSha = core.getInput('commit_sha')
+		const personalAccessToken = core.getInput('personal_access_token')
+		const owner = core.getInput('owner')
+		const repo = core.getInput('repo')
+		const label = core.getInput('label')
+		const forceUpdateBuildCountName = core.getInput(
+			'force_update_build_count_name'
+		)
 
-    core.debug(`Commit SHA: ${commitSha}`)
-    core.debug(`Owner: ${owner}`)
-    core.debug(`Repo: ${repo}`)
-    core.debug(`Label: ${label}`)
-    core.debug(`Force Update Build Count Name: ${forceUpdateBuildCountName}`)
+		core.debug(`Commit SHA: ${commitSha}`)
+		core.debug(`Owner: ${owner}`)
+		core.debug(`Repo: ${repo}`)
+		core.debug(`Label: ${label}`)
+		core.debug(`Force Update Build Count Name: ${forceUpdateBuildCountName}`)
 
-    const octokit = new Octokit({
-      auth: personalAccessToken
-    })
+		const octokit = new Octokit({
+			auth: personalAccessToken
+		})
 
-    // Make request to get pull requests associated with the commit
-    core.debug(`Fetching pull requests for commit ${commitSha}...`)
-    const getPullRequestsResponse =
-      await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
-        owner,
-        repo,
-        commit_sha: commitSha
-      })
-    // Filter pull requests by label
-    const pullRequests = getPullRequestsResponse.data.filter((pr) => {
-      if (pr.labels.length === 0) {
-        return false
-      }
-      return Boolean(pr.labels.find((prLabel) => prLabel.name === label))
-    })
+		core.debug(
+			`Fetching repository variable with the name ${forceUpdateBuildCountName}...`
+		)
+		const getRepositoryVariableResponse =
+			await octokit.rest.actions.getRepoVariable({
+				owner,
+				repo,
+				name: forceUpdateBuildCountName
+			})
+		const forceUpdateBuildCount = getRepositoryVariableResponse.data.value
 
-    const getRepositoryVariableResponse =
-      await octokit.rest.actions.getRepoVariable({
-        owner,
-        repo,
-        name: forceUpdateBuildCountName
-      })
-    const forceUpdateBuildCount = getRepositoryVariableResponse.data.value
+		if (!forceUpdateBuildCount) {
+			core.setOutput('force_update_build_count', 0)
+			return
+		}
 
-    if (!forceUpdateBuildCount) {
-      core.setOutput('force_update_build_count', 0)
-      return
-    }
+		// Make request to get pull requests associated with the commit
+		core.debug(`Fetching pull requests for commit ${commitSha}...`)
 
-    core.debug(
-      `Force update build count: ${forceUpdateBuildCount} (from variable ${forceUpdateBuildCountName})`
-    )
-    core.debug(
-      `Found ${pullRequests.length} pull requests associated with the commit`
-    )
+		let pullRequests = []
 
-    // If there are no pull requests associated with the commit, set the output to current force update build count.
-    if (pullRequests.length === 0) {
-      core.setOutput('force_update_build_count', forceUpdateBuildCount)
-      return
-    }
+		try {
+			const getPullRequestsResponse =
+				await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+					owner,
+					repo,
+					commit_sha: commitSha
+				})
+			// Filter pull requests by label
+			pullRequests = getPullRequestsResponse.data.filter((pr) => {
+				if (pr.labels.length === 0) {
+					return false
+				}
+				return Boolean(pr.labels.find((prLabel) => prLabel.name === label))
+			})
+		} catch (err) {
+			if (err instanceof Error) {
+				core.error(err)
+			}
+			// If there's an error when fetching the pull requests, set the `force_update_build_count` output to the current value.
+			core.setOutput('force_update_build_count', forceUpdateBuildCount)
+			return
+		}
 
-    const latestForceUpdateBuildCount = (
-      parseInt(forceUpdateBuildCount) + 1
-    ).toString()
-    // Update the force update build count variable
-    await octokit.rest.actions.updateRepoVariable({
-      owner,
-      repo,
-      name: forceUpdateBuildCountName,
-      value: latestForceUpdateBuildCount
-    })
-    core.setOutput('force_update_build_count', latestForceUpdateBuildCount)
-  } catch (error) {
-    if (error instanceof Error) {
-      core.error(error)
-    }
+		core.info(
+			`Force update build count: ${forceUpdateBuildCount} (from variable ${forceUpdateBuildCountName})`
+		)
+		core.info(
+			`Found ${pullRequests.length} pull requests associated with the commit`
+		)
 
-    // Avoid failing the workflow run if there is an error occurring in the action. Instead, set output to 0.
-    core.setOutput('force_update_build_count', 0)
-  }
+		// If there are no pull requests associated with the commit, set the output to current force update build count.
+		if (pullRequests.length === 0) {
+			core.setOutput('force_update_build_count', forceUpdateBuildCount)
+			return
+		}
+
+		const latestForceUpdateBuildCount = (
+			parseInt(forceUpdateBuildCount) + 1
+		).toString()
+
+		try {
+			// Update the force update build count variable
+			await octokit.rest.actions.updateRepoVariable({
+				owner,
+				repo,
+				name: forceUpdateBuildCountName,
+				value: latestForceUpdateBuildCount
+			})
+		} catch {
+			// If there's an error when patching the repository variable, set the `force_update_build_count` output to the current value.
+			core.setOutput('force_update_build_count', forceUpdateBuildCount)
+			return
+		}
+
+		core.setOutput('force_update_build_count', latestForceUpdateBuildCount)
+	} catch (error) {
+		if (error instanceof Error) {
+			core.error(error)
+		}
+		// If there is an error in request setup, fetching repository variable, or computing the updated force build count,
+		// then set the ouput to `NaN`.
+		core.setOutput('force_update_build_count', NaN)
+	}
 }
